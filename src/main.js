f878fdeb9f03716c6057ee5298488886c31f06da
unknown <zf@ituji.cn>
2018-05-15 15:23:29 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端点击事件延迟300毫秒解决方案
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 两三倍屏1px像素解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>home</div>
+  <div>hello world</div>
 </template>
 
 <script>
