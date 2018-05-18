06518d84ae33ca0ccddfa154b3b8204faf7d0652
Rice-F <2604211051@qq.com>
2018-05-18 22:57:31 +0800

    finish swiper

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import App from './App'
 import router from './router'
 // 移动端点击事件延迟300毫秒解决方案
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import 'styles/reset.css'
 // 两三倍屏1px像素解决方案
 import 'styles/border.css'
 // iconfont
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-image" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<style lang="stylus" scoped>
+  .
+  wrapper > > >
+  .swiper-pagination-bullet-active
+    background #fff
+
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 31.25%
+    background #eee
+    .swiper-image
+      width 100%
+</style>
+<script>
+export default{
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 1000
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'https://img1.qunarzz.com/qs/1805/95/f650b837b5246302.jpg'
+        }, {
+          id: '002',
+          imgUrl: 'https://img1.qunarzz.com/qs/1805/f6/ed3f589004da902.jpg'
+        }
+      ]
+    }
+  }
+}
+
+</script>
