d297bb846c1e96998371668b3d28f701d095251e
unknown <zf@ituji.cn>
2018-05-22 16:07:09 +0800

    finish home

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -18,35 +18,37 @@
     .icons >>> .swiper-container
       height 0
       padding-bottom 50%
-    .icon
-      position relative
-      float left
-      width 25%
-      height 0
-      padding-bottom 25%
-      overflow hidden
-      .icon-image
-        position absolute
-        top: 0
-        left 0
-        right 0
-        bottom .1rem
-        box-sizing border-box
-        padding .1rem
-        .icon-image-content
-          display block
-          height 100%
-          margin 0 auto
-      .icon-desc
-        position absolute
-        bottom 0
-        left 0
-        right 0
-        height .44rem
-        line-height .44rem
-        color $darkTestColor
-        text-align center
-        ellipsis()
+    .icons
+      margin-top .1rem
+      .icon
+        position relative
+        float left
+        width 25%
+        height 0
+        padding-bottom 25%
+        overflow hidden
+        .icon-image
+          position absolute
+          top: 0
+          left 0
+          right 0
+          bottom .3rem
+          box-sizing border-box
+          padding .1rem
+          .icon-image-content
+            display block
+            height 100%
+            margin 0 auto
+        .icon-desc
+          position absolute
+          bottom 0
+          left 0
+          right 0
+          height .44rem
+          line-height .44rem
+          color $darkTestColor
+          text-align center
+          ellipsis()
 </style>
 <script>
 export default{
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">
+      周末去哪呀
+    </div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl"/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/a4/df8543b6b8506d.jpg_r_640x214_660e1e3a.jpg',
+          title: '就是要看海',
+          desc: '海上摩托，拖曳伞什么的爽歪歪了'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/a4/df8543b6b8506d.jpg_r_640x214_660e1e3a.jpg',
+          title: '就是要看海',
+          desc: '海上摩托，拖曳伞什么的爽歪歪了'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/a4/df8543b6b8506d.jpg_r_640x214_660e1e3a.jpg',
+          title: '就是要看海',
+          desc: '海上摩托，拖曳伞什么的爽歪歪了'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+  .item-img
+    width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      font-size .28rem
+      color #ccc
+      ellipsis()
+</style>
