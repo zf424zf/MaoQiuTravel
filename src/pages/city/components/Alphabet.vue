335dcda8f677ead0abb197189509163407bb19e9
unknown <zf@ituji.cn>
2018-06-07 00:11:20 +0800

    city components finish some

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
 <ul class="list">
-  <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+  <li class="item"
+      v-for="(item, key) of cities"
+      :key="key"
+      @click="handleClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      :ref="key"
+  >{{key}}</li>
 </ul>
 </template>
 
@@ -9,6 +17,42 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleClick (e) {
+      const letter = e.target.innerText
+      this.$emit('clickLetter', letter)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = (Math.floor(touchY - startY)) / 20
+        if (index >= 0 && index <= this.letters.length) {
+          this.$emit('clickLetter', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
   }
 }
 </script>
