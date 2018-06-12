887ba4f6f9721ceab80315d5a13019cd84254f75
zf424zf <704273241@qq.com>
2018-06-12 23:03:52 +0800

    detail banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_600x330_dfb694b6.jpg"/>
+      <div class="banner-info">
+        <div class="banner-title">南京总统府(AAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          11
+        </div>
+      </div>
+    </div>
+    <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_r_800x800_50c4f278.jpg',
+        'http://img1.qunarzz.com/sight/p0/1508/64/642f83bc418217bf.img.jpg_r_800x800_52e82e5b.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .32rem
+        line-height .4rem
+        margin-top .14rem
+        padding 0 .4rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
