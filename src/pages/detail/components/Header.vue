14cc8e2412e0b69c2a93ea08556054d50b481167
zf424zf <704273241@qq.com>
2018-09-10 20:55:34 +0800

    remove global event

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -40,6 +40,10 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    // 解绑全局事件
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
