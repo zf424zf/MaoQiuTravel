b7d6044ca0e08d14101ed3cb987bc5fa463b768a
zf424zf <704273241@qq.com>
2018-09-11 21:57:54 +0800

    detail ajax & add route scrollBehavior

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName" :bannerImage="bannerImage" :bannerImages="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -12,6 +12,7 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 
 export default {
   name: 'Detail',
@@ -22,30 +23,39 @@ export default {
   },
   data () {
     return {
-      list: [
-        {
-          title: '成人票',
-          children: [
-            {
-              title: '成人一馆联票',
-              children: [
-                {title: '成人一馆联票 - 阿毛肉联厂销售'}
-              ]
-            },
-            {title: '成人三馆联票'},
-            {title: '成人五馆联票'}
-          ]
-        },
-        {title: '学生票'},
-        {title: '儿童票'},
-        {title: '优惠票'}
-      ]
+      sightName: '',
+      bannerImage: '',
+      gallaryImgs: [],
+      list: []
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImage = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+        console.log(data)
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.content
-  height 50rem
+  .content
+    height 50rem
 </style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_600x330_dfb694b6.jpg"/>
+      <img class="banner-img" :src="bannerImage"/>
       <div class="banner-info">
-        <div class="banner-title">南京总统府(AAAA景区)</div>
+        <div class="banner-title">{{sightName}}</div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe692;</span>
-          11
+          {{this.bannerImages.length}}
         </div>
       </div>
     </div>
-    <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+    <common-gallery :imgs="bannerImages" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
   </div>
 </template>
 
@@ -18,11 +18,14 @@
 import CommonGallery from 'common/gallery/Gallery'
 export default {
   name: 'DetailBanner',
+  props: {
+    sightName: String,
+    bannerImage: String,
+    bannerImages: Array
+  },
   data () {
     return {
-      showGallery: false,
-      imgs: ['http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_r_800x800_50c4f278.jpg',
-        'http://img1.qunarzz.com/sight/p0/1508/64/642f83bc418217bf.img.jpg_r_800x800_52e82e5b.jpg']
+      showGallery: false
     }
   },
   methods: {
