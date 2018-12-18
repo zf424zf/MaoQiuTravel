e3fa94ede225305c36729f89495563782cda9117
zf424zf <704273241@qq.com>
2018-12-18 20:54:30 +0800

    add use count header

---
+++ src/pages/usecount/UseCount.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      father
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UseCount"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/usecount/UserCountHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>header</div>
+</template>
+
+<script>
+    export default {
+        name: "UserCountHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
